c350de1fb6950d7c122e9326fab49a819c8ae974
Dang Hoang Vu <danghvu@gmail.com>
2013-03-04 12:14:17 +0100

    fix minor bug with initApp not ready

--- index.php
+++ index.php
@@ -80,52 +80,7 @@ while(tags.length)
   </head>
   <body>
     <div id="fb-root"></div>
-    <script type="text/javascript">
-        window.fbAsyncInit = function() {
-            FB.init({
-                appId      : '<?php echo AppInfo::appID(); ?>', // App ID
-                channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
-                status     : true, // check login status
-                cookie     : true, // enable cookies to allow the server to access the session
-                xfbml      : true // parse XFBML
-            });
-
-            // Listen to the auth.login which will be called when the user logs in
-            // using the Login button
-            FB.Event.subscribe('auth.login', function(response) {
-                // We want to reload the page now so PHP can read the cookie that the
-                // Javascript SDK sat. But we don't want to use
-
-                // just in case need additional permission, do not reload but force click login button
-                // maybe switch to on-demand later
-                var perms = ['read_stream', 'publish_actions'];
-
-                FB.api('/me/permissions', function (response) {
-                    for (var i=0;i<perms.length;i++){ 
-                        if (!response.data[0][perms[i]]) {
-                            return;
-                        }
-                    }
-                    window.location = window.location;
-                });
-            });
-
-            //load_no_comment();
-            initApp("<?php echo isset($accessToken)? $accessToken:''; ?>");
-
-            FB.Canvas.setAutoGrow();
-        };
-
-        // Load the SDK Asynchronously
-        (function(d, s, id) {
-            var js, fjs = d.getElementsByTagName(s)[0];
-            if (d.getElementById(id)) return;
-            js = d.createElement(s); js.id = id;
-            js.src = "//connect.facebook.net/en_US/all.js";
-            fjs.parentNode.insertBefore(js, fjs);
-        }(document, 'script', 'facebook-jssdk'));
-    </script>
-
+    
     <header class="clearfix">
       <?php if (isset($basic)) { ?>
       <p id="picture" style="background-image: url(https://graph.facebook.com/<?php echo he($user_id); ?>/picture?type=normal)"></p>
@@ -327,6 +282,52 @@ function submit_comment(post_id) {
   </li>
 {/friends}
 </ul>
+</script>
+
+<script type="text/javascript">
+        window.fbAsyncInit = function() {
+            FB.init({
+                appId      : '<?php echo AppInfo::appID(); ?>', // App ID
+                channelUrl : '//<?php echo $_SERVER["HTTP_HOST"]; ?>/channel.html', // Channel File
+                status     : true, // check login status
+                cookie     : true, // enable cookies to allow the server to access the session
+                xfbml      : true // parse XFBML
+            });
+
+            // Listen to the auth.login which will be called when the user logs in
+            // using the Login button
+            FB.Event.subscribe('auth.login', function(response) {
+                // We want to reload the page now so PHP can read the cookie that the
+                // Javascript SDK sat. But we don't want to use
+
+                // just in case need additional permission, do not reload but force click login button
+                // maybe switch to on-demand later
+                var perms = ['read_stream', 'publish_actions'];
+
+                FB.api('/me/permissions', function (response) {
+                    for (var i=0;i<perms.length;i++){ 
+                        if (!response.data[0][perms[i]]) {
+                            return;
+                        }
+                    }
+                    window.location = window.location;
+                });
+            });
+
+            //load_no_comment();
+            initApp("<?php echo isset($accessToken)? $accessToken:''; ?>");
+
+            FB.Canvas.setAutoGrow();
+        };
+
+        // Load the SDK Asynchronously
+        (function(d, s, id) {
+            var js, fjs = d.getElementsByTagName(s)[0];
+            if (d.getElementById(id)) return;
+            js = d.createElement(s); js.id = id;
+            js.src = "//connect.facebook.net/en_US/all.js";
+            fjs.parentNode.insertBefore(js, fjs);
+        }(document, 'script', 'facebook-jssdk'));
 </script>
 
   </body>
